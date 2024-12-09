f7920ecdddaa943f299b09c6d420a4ff43a7aeb6
JeremieDup <113355449+JeremieDup@users.noreply.github.com>
2024-12-09 19:05:49 +0100

    feat : Create Web configuration

---
+++ JeremAirlines/src/main/java/org/example/jeremairlines/JeremAirlinesApplication.java
@@ -0,0 +1,11 @@
+package org.example.jeremairlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeremAirlinesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JeremAirlinesApplication.class, args);
+    }
+}
